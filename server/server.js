adb12faed989167d918010f7fcbf4d9d4a0bb831
Alec Dewitz <atdewitz@csbsju.edu>
2018-03-09 21:54:01 -0600

    Updated to latest versions and compatibility

--- server/server.js
+++ server/server.js
@@ -1,29 +1,38 @@
 'use strict';
 
-let db
-let dbconnection = 'mongodb://reptilehaus_u:reptilehaus_p@ds035026.mlab.com:35026/die-reptil'
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+let db;
+let dbconnection = 'mongodb://localhost';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 MongoClient.connect(dbconnection, (err, database) => {
-    if (err) return console.log(err)
-    db = database
+    if (err) {
+        return console.log(err);
+    }
+    db = database;
     app.listen(3000, () => {
         console.log('MONGODB listening on 3000')
-    })
-})
+    });
+});
 
 
 function databaseStore(message) {
-    let storeData = { chatMessage: message, timestamp: new Date().getTime() }
+    let storeData = {
+        chatMessage: message,
+        timestamp: new Date().getTime()
+    }
     db.collection('chatroom-chats').save(storeData, (err, result) => {
-        if (err) return console.log(err)
+        if (err) {
+            return console.log(err);
+        }
         console.log('saved to database')
     })
 }
@@ -40,12 +49,15 @@ io.on('connection', (socket) => {
 
     console.log('user connected');
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
     socket.on('add-message', (message) => {
-        io.emit('message', { type: 'new-message', text: message });
+        io.emit('message', {
+            type: 'new-message',
+            text: message
+        });
         // Function above that stores the message in the database
         databaseStore(message)
     });
